1c616f92bcbc27d82d6909c20c23db5963d0ffbe
rk-Confectionery <129360121+rk-Confectionery@users.noreply.github.com>
2023-04-09 21:20:10 +0530

    Add files via upload

---
+++ components/Layout.js
@@ -0,0 +1,202 @@
+import { signOut, useSession } from "next-auth/react";
+import Head from "next/head";
+import Link from "next/link";
+import Cookies from "js-cookie";
+import React, { useContext, useEffect, useState } from "react";
+import { ToastContainer } from "react-toastify";
+import { Menu } from "@headlessui/react";
+import "react-toastify/dist/ReactToastify.css";
+import { Store } from "../utils/Store";
+import DropdownLink from "./DropdownLink";
+import { useRouter } from "next/router";
+import { SearchIcon } from "@heroicons/react/outline";
+import {
+  HomeIcon,
+  ShoppingCartIcon,
+  CubeIcon,
+  ViewColumnsIcon,
+  BarsArrowDownIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/outline";
+
+export default function Layout({ title, children }) {
+  const { status, data: session } = useSession();
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const [cartItemsCount, setCartItemsCount] = useState(0);
+  useEffect(() => {
+    setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+  }, [cart.cartItems]);
+  const [sidbarVisible, setSidebarVisible] = useState(false);
+  const sidebarOpenHandler = () => {
+    setSidebarVisible(true);
+  };
+  const sidebarCloseHandler = () => {
+    setSidebarVisible(false);
+  };
+  const [query, setQuery] = useState("");
+
+  const router = useRouter();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    router.push(`/search?query=${query}`);
+  };
+
+  const logoutClickHandler = () => {
+    Cookies.remove("cart");
+    dispatch({ type: "CART_RESET" });
+    signOut({ callbackUrl: "/login" });
+  };
+  return (
+    <>
+      <Head>
+        <title>{title ? title : "rk Confectionery"}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <ToastContainer position="bottom-center" limit={1} />
+
+      <div className="flex min-h-screen flex-col justify-between " id="search">
+        <header className="sticky top-0 z-50 bg-white">
+          <nav className="flex  h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">RK Confectionery</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">
+                  Cart
+                  {cartItemsCount > 0 && (
+                    <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </a>
+              </Link>
+
+              {status === "loading" ? (
+                "Loading"
+              ) : session?.user ? (
+                <Menu as="div" className="relative inline-block hh">
+                  <Menu.Button className="text-blue-600">
+                    {session.user.name}
+                  </Menu.Button>
+                  <Menu.Items className="absolute right-0 w-56 origin-top-right bg-white  shadow-lg ">
+                    <Menu.Item>
+                      <DropdownLink className="dropdown-link" href="/profile">
+                        Profile
+                      </DropdownLink>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <DropdownLink
+                        className="dropdown-link"
+                        href="/order-history"
+                      >
+                        Order History
+                      </DropdownLink>
+                    </Menu.Item>
+                    {session.user.isAdmin && (
+                      <Menu.Item>
+                        <DropdownLink
+                          className="dropdown-link"
+                          href="/admin/dashboard"
+                        >
+                          Admin Dashboard
+                        </DropdownLink>
+                      </Menu.Item>
+                    )}
+                    <Menu.Item>
+                      <a
+                        className="dropdown-link"
+                        href="#"
+                        onClick={logoutClickHandler}
+                      >
+                        Logout
+                      </a>
+                    </Menu.Item>
+                  </Menu.Items>
+                </Menu>
+              ) : (
+                <Link href="/login">
+                  <a className="p-2">Login</a>
+                </Link>
+              )}
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <div className="md:invisible flex flex-nowrap content-evenly justify-between sticky bg-white bottom-0  h-12 shadow-inner my-4">
+          <div className="basis-1/3 flex  flex-col justify-items-center items-center  align-center justify-center">
+            <Link href="/">
+              <a className="bg-white">
+                <HomeIcon class="w-8 h-6 text-grey-100" />
+                <p className="text-xs text-grey-600">Home</p>
+              </a>
+            </Link>
+          </div>
+          <div className="basis-1/3 flex  flex-col justify-items-center items-center  align-center justify-center">
+            <Link href="/cart">
+              <a className="p-2">
+                <ShoppingCartIcon className="w-8 h-6 text-grey-600" />
+                <p className="text-xs text-grey-600">Cart</p>
+              </a>
+            </Link>
+          </div>
+          <div className="basis-1/3 flex  flex-col justify-items-center items-center  align-center justify-center">
+            <Link href="/order-history">
+              <a>
+                <CubeIcon className="w-8 h-6 text-grey-600" />
+                <p className="text-xs text-grey-600">Orders</p>
+              </a>
+            </Link>
+          </div>
+          <div className="basis-1/3 flex  flex-col justify-items-center items-center  align-center justify-center">
+            <Link href="/search1#search">
+              <a>
+                <svg
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                  aria-hidden="true"
+                  className="w-10 h-6 font-bold"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                  ></path>
+                </svg>
+                <p className="text-xs text-grey-600">Search</p>
+              </a>
+            </Link>
+          </div>
+
+          <Link href="/#category">
+            <a className="basis-1/3 flex  flex-col justify-items-center items-center  align-center justify-center">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                className="w-10 h-6 font-bold"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+                ></path>
+              </svg>
+              <p className="text-xs text-grey-600">Categories</p>
+            </a>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ pages/shipping.js
@@ -0,0 +1,203 @@
+import React, { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import Cookies from "js-cookie";
+import CheckoutWizard from "../components/CheckoutWizard";
+import Layout from "../components/Layout";
+import { Store } from "../utils/Store";
+import { useRouter } from "next/router";
+
+export default function ShippingScreen() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress } = cart;
+  const router = useRouter();
+
+  useEffect(() => {
+    setValue("fullName", shippingAddress.fullName);
+    setValue("address", shippingAddress.address);
+    setValue("city", shippingAddress.city);
+    setValue("postalCode", shippingAddress.postalCode);
+    setValue("country", shippingAddress.country);
+    setValue("number", shippingAddress.number);
+  }, [setValue, shippingAddress]);
+
+  const submitHandler = ({
+    fullName,
+    address,
+    city,
+    postalCode,
+    country,
+    number,
+  }) => {
+    dispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: { fullName, address, city, postalCode, country, number },
+    });
+    Cookies.set(
+      "cart",
+      JSON.stringify({
+        ...cart,
+        shippingAddress: {
+          fullName,
+          address,
+          city,
+          postalCode,
+          country,
+          number,
+        },
+      })
+    );
+
+    router.push("/payment");
+  };
+
+  return (
+    <Layout title="Shipping Address">
+      <CheckoutWizard activeStep={1} />
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Shipping Address</h1>
+        <div className="mb-4">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            className="w-full"
+            id="fullName"
+            autoFocus
+            {...register("fullName", {
+              required: "Please enter full name",
+            })}
+          />
+          {errors.fullName && (
+            <div className="text-red-500">{errors.fullName.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="number">Phone Number</label>
+          <input
+            type="tel"
+            className="w-full"
+            id="number"
+            pattern="[0-9]{10}"
+            {...register("number", {
+              required: "Please enter phone number",
+            })}
+          />
+          {errors.number && (
+            <div className="text-red-500">{errors.number.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="address">Address</label>
+          <input
+            className="w-full"
+            id="address"
+            {...register("address", {
+              required: "Please enter address",
+              minLength: { value: 3, message: "Address is more than 2 chars" },
+            })}
+          />
+          {errors.address && (
+            <div className="text-red-500">{errors.address.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">City</label>
+          <select
+            name="city"
+            id="city"
+            value="Deoband"
+            className="w-full"
+            {...register("city", {
+              required: "Please enter city",
+            })}
+          >
+            <option value="Deoband" selected>
+              Deoband
+            </option>
+          </select>
+          {errors.city && (
+            <div className="text-red-500 ">{errors.city.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="postalCode">PIN Code</label>
+          <select
+            name="postalCode"
+            value="247554"
+            id="postalCode"
+            className="w-full"
+            {...register("postalCode", {
+              required: "Please enter postalCode",
+            })}
+          >
+            <option value="247554" selected>
+              247554
+            </option>
+          </select>
+          {errors.postalCode && (
+            <div className="text-red-500 ">{errors.postalCode.message}</div>
+          )}
+        </div>
+        {/* <div className="mb-4">
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            className="w-full"
+            id="postalCode"
+            {...register("postalCode", {
+              required: "Please enter postal code",
+            })}
+          />
+          {errors.postalCode && (
+            <div className="text-red-500 ">{errors.postalCode.message}</div>
+          )}
+        </div> */}
+        <div className="mb-4">
+          <label htmlFor="country">Country</label>
+          <select
+            value="India"
+            name="country"
+            id="country"
+            className="w-full"
+            {...register("country", {
+              required: "Please enter country",
+            })}
+          >
+            <option value="India" selected>
+              India
+            </option>
+          </select>
+          {errors.country && (
+            <div className="text-red-500 ">{errors.country.message}</div>
+          )}
+        </div>
+        {/* <div className="mb-4">
+          <label htmlFor="country">Country</label>
+          <input
+            className="w-full"
+            id="country"
+            {...register("country", {
+              required: "Please enter country",
+            })}
+          />
+          {errors.country && (
+            <div className="text-red-500 ">{errors.country.message}</div>
+          )}
+        </div> */}
+        <div className="mb-4 flex justify-between">
+          <button className="primary-button">Next</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
+
+ShippingScreen.auth = true;
